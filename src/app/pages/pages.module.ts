94e58e90bc726bd914c1ba7c37d703c3cfa66a2d
Jocelyn Diaz <jocelyn.diaz@bmore.systems>
2020-01-21 15:01:38 -0600

    progress component

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,12 +3,19 @@ import { NgModule } from "@angular/core";
 // Modulos
 import { SharedModule } from '../shared/shared.module';
 
+import { FormsModule } from '@angular/forms';
+
+// ng2-charts
+import { ChartsModule } from 'ng2-charts';
+
 import { PagesComponent } from './pages.component';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Graficas1Component } from './graficas1/graficas1.component';
 import { PAGES_ROUTES } from './pages.routes';
+import { IncrementadorComponent } from '../components/incrementador/incrementador.component';
+
 
 
 
@@ -18,6 +25,8 @@ import { PAGES_ROUTES } from './pages.routes';
         DashboardComponent,
         ProgressComponent,
         Graficas1Component,
+        IncrementadorComponent,
+
     ],
     exports:[
         DashboardComponent,
@@ -26,6 +35,8 @@ import { PAGES_ROUTES } from './pages.routes';
     ],
     imports:[
         SharedModule,
+        FormsModule,
+        ChartsModule,
         PAGES_ROUTES
     ]
 
